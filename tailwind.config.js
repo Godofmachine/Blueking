7da8a90fec7b1448085ea237ec99325c5a55e072
Samuel Adeniran <127601699+Godofmachine@users.noreply.github.com>
2023-10-22 05:38:39 +0100

    fix commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/*.html", "./node_modules/flowbite/**/*.js"],
+  content: ["./build/**/*.html", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
   },
   plugins: [
     // require('flowbite/plugin')
-],
-}
+  ],
+};
